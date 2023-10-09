4bb692193a7bcb06fa7472c704a11cf397e9f397
AshThrax <PSR10894@students.ephec.be>
2023-10-09 16:15:08 +0200

    mise en place du fichier json a destination de l'acceuil

--- jacksDog/src/app/acceuil/accueil/accueil.component.ts
+++ jacksDog/src/app/acceuil/accueil/accueil.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
 @Component({
   selector: 'app-accueil',
   templateUrl: './accueil.component.html',
   styleUrls: ['./accueil.component.css']
 })
-export class AccueilComponent {
+export class AccueilComponent implements OnInit {
+  public AccueilInfo: any;
+  constructor(private http: HttpClient)
+  {
+
+  }
+  ngOnInit(): void {
+   const url:string ='assets/json/acceuil.json';
+   this.http.get(url).subscribe(res=> {
+    this.AccueilInfo=res
+   }
+);}
 
 }
